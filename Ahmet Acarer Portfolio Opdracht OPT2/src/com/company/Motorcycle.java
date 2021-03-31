4e376dd70a79ada3589ea7d6730c74d23f420972
Ahmet Acarer <73262708+AhmetAcarer@users.noreply.github.com>
2021-03-31 20:30:43 +0200

    Delete Ahmet Acarer Portfolio Opdracht OPT2 directory

--- Ahmet Acarer Portfolio Opdracht OPT2/src/com/company/Motorcycle.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Motorcycle extends Vehicle{
-    public Motorcycle(String name, int weight, int price, int seatingCapacity, int kmPerLiter, String fuelType) {
-        super(name, weight, price, seatingCapacity, kmPerLiter, fuelType);
-    }
-
-}
--- Ahmet Acarer Portfolio Opdracht OPT2/src/com/company/Truck.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Truck extends Vehicle{
-    public Truck(String name, int weight, int price, int seatingCapacity, int kmPerLiter, String fuelType) {
-        super(name, weight, price, seatingCapacity, kmPerLiter, fuelType);
-    }
-}
